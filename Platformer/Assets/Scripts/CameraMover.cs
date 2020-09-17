6e1ff6110e808d7e04fcca6bdeceebba4e4a3d4a
SamPfister <54378666+SamPfister@users.noreply.github.com>
2020-09-17 08:22:28 -0500

    First Commit
    This is as of the end of part 5 of the tutorial

---
+++ Platformer/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    public bool useOffsetValues;
+
+    public float mouseSens;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get x pos of mmouse and rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * mouseSens;
+        float vertical = Input.GetAxis("Mouse Y") * mouseSens;
+        target.Rotate(-vertical, horizontal, 0);
+        //move camera based on rotation of target and orig. offset
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = target.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation*offset);
+
+        //target.Rotate(0, horizontal, vertical);
+        //transform.position = target.position - offset;
+        transform.LookAt(target);
+    }
+}
---
+++ Platformer/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    private Vector3 moveDirection;
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+    public float gravityScale;
+
+    void Start()
+    {
+
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+        moveDirection.y = moveDirection.y + Physics.gravity.y * gravityScale * Time.deltaTime;
+        controller.Move(moveDirection*Time.deltaTime);
+    }
+}
