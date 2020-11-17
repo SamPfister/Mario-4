758a723de2b69971a82df9d3f099d2832c8add2c
SamPfister <54378666+SamPfister@users.noreply.github.com>
2020-11-17 08:06:12 -0600

    particles
    wow

--- Assets/Scripts/GoldPickup.cs
+++ Assets/Scripts/GoldPickup.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class GoldPickup : MonoBehaviour
 {
 
     public int value;
-
+    public GameObject pickupEffect;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +25,7 @@ public class GoldPickup : MonoBehaviour
         if(other.tag == "Player")
         {
             FindObjectOfType<GameManager>().AddGold(value);
+            Instantiate(pickupEffect, transform.position, transform.rotation);
             Destroy(gameObject);
         }
     }
