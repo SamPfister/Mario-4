6514cc0cffe9e0dde45b6e6993913d7859009a1e
SamPfister <54378666+SamPfister@users.noreply.github.com>
2021-01-17 17:10:42 -0600

    more additions
    added correct jumping and fixed issue from yesterday

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,54 +5,76 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody playerBody;
-    public float moveSpeed = 50f;
-    public float maxSpeed = 20f;
-    public float jumpForce = 80f;
-    public Vector3 movement;
+    public float moveSpeed;
+    public float maxSpeed;
+    public float jumpForce; 
     public Vector3 playerGravity;
+    public bool isGrounded;
+    
  
     // Start is called before the first frame update
     void Start()
     {
+ 
         playerBody = GetComponent<Rigidbody>();
-        playerBody.useGravity = true;
+
         //this is what controls the gravity on the player
-        playerGravity = new Vector3(0f, -5f, 0f);
-        
-    }
+        playerGravity = new Vector3(0f, -6f, 0f);
+ 
+        moveSpeed = 100f;
+        maxSpeed = 20f;
+        jumpForce = 80f;
+
+}
 
     // Update is called once per frame
     void Update()
     {
+        // credits to this link ofr the movement input stuff https://answers.unity.com/questions/1445397/why-is-it-so-hard-to-make-a-rigidbody-jump-in-the.html
+        // and this one for the x and z speed cap https://answers.unity.com/questions/772165/constrain-velocity-on-only-x-z-let-the-jump-fly-fr.html
 
-        //Bug to fix: Player gravity while jumping works normally when no input held after starting jump, but moves down linearlly if input held
-        //this is probably something to do with the speed limiter capping total magnitude of vector3
-        // if it could only cap x and z magnitudes and leave y alone that would be cool
+        if (isGrounded)
+        {
+            moveSpeed = 100f;
+        }
+        else
+        {
+            moveSpeed = 10f;
+        }
 
-        // credits to this link https://answers.unity.com/questions/1445397/why-is-it-so-hard-to-make-a-rigidbody-jump-in-the.html
-        //Movement input
+
         float strafe = Input.GetAxisRaw("Horizontal");
         float movement = Input.GetAxisRaw("Vertical");
 
         Vector3 newVel = transform.forward * movement + transform.right * strafe;
         newVel = newVel.normalized * moveSpeed;
 
-        //speed limiter
-        if (playerBody.velocity.magnitude > maxSpeed)
+        //speed limiter, leaves y unaffected
+        Vector3 temp = newVel;
+        float tempy = temp.y;
+        temp.y = 0;
+        temp = Vector3.ClampMagnitude(temp, maxSpeed);
+        temp.y = tempy;
+        newVel = temp;
+        
+        
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody>().velocity = Vector3.ClampMagnitude(GetComponent<Rigidbody>().velocity, maxSpeed);
+            playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse );
         }
 
         playerBody.AddForce(newVel + playerGravity);
 
-        
-        if (Input.GetButtonDown("Jump"))
-        {
-            playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse );
-        }
     }
-    void FixedUpdate()
-    {    
-        //playerBody.AddForce(playerGravity);
+
+    //These two methods are used for checking if the player is colliding with something
+    // used to limit ability of player to air-strafe
+    void OnCollisionEnter(Collision other)
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit(Collision other)
+    {
+        isGrounded = false;
     }
 }
